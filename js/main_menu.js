61de1c6121d1e55042000e6c6ae921206cef5fe0
jroo <ruihley@gmail.com>
2010-11-30 21:14:18 -0500

    update main menu title

--- js/main_menu.js
+++ js/main_menu.js
@@ -3,7 +3,7 @@ MainMenuView.prototype = new View();
 function MainMenuView() {
     var self = this;
     self.containerDiv = 'main_menu_body';
-    self.titleString = 'Menu';
+    self.titleString = 'New START';
 
     self.render = function() {
         self.setTitle(self.titleString);
